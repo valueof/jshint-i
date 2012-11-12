683a44060bbb2602fccf0d19becf59380bea367c
Anton Kovalyov <anton@kovalyov.net>
2012-11-12 13:33:43 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+jshint-i (for interactive) is a small CLI tool that allows
+you to go over your JSHint report interactively.
+
+You can install it from NPM:
+
+    $ npm install jshint-i
+    $ jshint-i myfile.js --config=myconfig.json
+
+
+![Screenshot](http://cl.ly/image/1e1L0B3p2K12/jshint-i.png)
---
+++ jshint-i.js
@@ -0,0 +1,88 @@
+var fs = require("fs");
+var JSHINT = require("jshint").JSHINT;
+
+var queue;
+
+function scan() {
+	var file, config;
+	var args = process.argv.slice(2);
+
+	args.forEach(function (arg) {
+		if (/^\-\-config\=(.+)/.test(arg)) {
+			config = arg.slice(9);
+		} else {
+			file = arg;
+		}
+	});
+
+	if (!file || !fs.existsSync(file)) {
+		console.log("No file provided or file doesn't exist.");
+		process.exit(2);
+	}
+	
+	if (config) {
+		if (!fs.existsSync(config)) {
+			console.log("Config file doesn't exist.");
+			process.exit(2);
+		}
+		
+		config = JSON.parse(fs.readFileSync(config));
+	}
+	
+	var source = fs.readFileSync(file, "utf-8");
+
+	if (JSHINT(source, config || {})) {
+		console.log("Your code passed JSHint!");
+		process.exit(0);
+	}
+
+	queue = JSHINT.errors;
+}
+
+function prompt() {
+	process.stdout.write("\n[n] Next; [r] Rescan; [q] Quit; Action: ");
+	process.stdin.resume();	
+}
+
+function next() {
+	if (queue.length === 0) {
+		console.log("All done!");
+		process.exit(0);
+	}
+	
+	var curr = queue.shift();
+	process.stdout.write("\u001B[2J\u001B[0;0f");
+
+	console.log("Line:", curr.line);
+	if (curr.character) {
+		console.log("Character:", curr.character);
+	}
+		
+	console.log("\n>", curr.evidence.replace(/^\s+/, ""));
+	console.log(curr.reason);
+	prompt();
+}
+
+process.stdin.on("data", function (cmd) {
+	cmd = cmd.toString().replace(/\s+/, "");
+	
+	switch (cmd) {
+		case "q":
+			process.exit(0);
+			break;
+		case "n":
+			process.stdin.pause();
+			next();
+			break;
+		case "r":
+			scan();
+			next();
+			break;
+		default:
+			process.stdin.pause();
+			prompt();
+	}
+});
+	
+scan();
+next();
